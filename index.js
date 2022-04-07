6a1b1be622381bf037fbe246082fb33cf7b35427
eleftheros <eleftheros_all@protonmail.com>
2022-04-07 20:29:56 +0300

    Connect to database

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 require('dotenv').config();
+const { connect } = require('./lib/db');
 const app = require('./app');
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running on port ${port}...`));
+connect()
+  .then(() => {
+    console.log('Database connection established.');
+    app.listen(port, () => console.log(`Server running on port ${port}...`));
+  })
+  .catch(e => {
+    console.log(e.message);
+    process.exit();
+  });
